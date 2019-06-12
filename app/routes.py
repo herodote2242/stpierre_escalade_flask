800801d4118f90bc27686d0d9f2d9f7cabab3f51
unknown <matthias.gil@hotmail.fr>
2019-06-12 14:40:26 +0200

    routes modified with 'methods' argument added

--- app/routes.py
+++ app/routes.py
@@ -4,26 +4,26 @@
 from flask import render_template, redirect
 from app import app
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def visit_index():
     return render_template('index.html')
 
-@app.route('/club')
+@app.route('/club', methods=['GET'])
 def visit_club():
     return render_template('club.html')
 
-@app.route('/media')
+@app.route('/media', methods=['GET'])
 def visit_media():
     return render_template('media.html')
 
-@app.route('/horaires')
+@app.route('/horaires', methods=['GET'])
 def visit_horaires():
     return render_template('horaires.html')
 
-@app.route('/ffme')
+@app.route('/ffme', methods=['GET'])
 def visit_ffme():
     return redirect('https://www.ffme69.fr')
 
-@app.route('/facebook')
+@app.route('/facebook', methods=['GET'])
 def visit_fb():
     return redirect('https://www.facebook.com/SaintPierreEscalade')
