fe06d29b34ed42bba3612821c612b9dc5ff3a6cc
unknown <matthias.gil@hotmail.fr>
2019-06-12 15:38:02 +0200

    requirements.txt added

--- app/routes.py
+++ app/routes.py
@@ -4,26 +4,26 @@
 from flask import render_template, redirect
 from app import app
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def visit_index():
     return render_template('index.html')
 
-@app.route('/club', methods=['GET'])
+@app.route('/club')
 def visit_club():
     return render_template('club.html')
 
-@app.route('/media', methods=['GET'])
+@app.route('/media')
 def visit_media():
     return render_template('media.html')
 
-@app.route('/horaires', methods=['GET'])
+@app.route('/horaires')
 def visit_horaires():
     return render_template('horaires.html')
 
-@app.route('/ffme', methods=['GET'])
+@app.route('/ffme')
 def visit_ffme():
     return redirect('https://www.ffme69.fr')
 
-@app.route('/facebook', methods=['GET'])
+@app.route('/facebook')
 def visit_fb():
     return redirect('https://www.facebook.com/SaintPierreEscalade')
